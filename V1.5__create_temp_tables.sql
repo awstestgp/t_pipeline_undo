a5c287c4af987cf501730ba45d7417cbcd29133b
awstestgp <41288121+awstestgp@users.noreply.github.com>
2018-07-17 09:08:37 +0530

    Create V1.5__create_temp_tables.sql

---
+++ V1.5__create_temp_tables.sql
@@ -0,0 +1,24 @@
+Create table emp_demo
+(
+emp_id number , 
+emp_name varchar2(10),
+emp_adddr varchar2(100)
+);
+
+Create table emp_per
+(
+emp_id number , 
+emp_name varchar2(10),
+emp_adddr varchar2(100),
+mobile_no number 
+);
+
+Create table emp_add
+(
+emp_id number , 
+emp_name varchar2(10),
+emp_adddr varchar2(100),
+mobile_no number ,
+addr_line varchar2(1000),
+addr_line2 varchar2(1000)
+);
